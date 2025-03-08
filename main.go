6b65417c1c40cdfb5ca9102773da7283fef1a683
salmaahmedaly <salma.ahmedaly@outlook.com>
2025-03-08 10:39:20 -0600

    fixing ccvi link

--- main.go
+++ main.go
@@ -1398,7 +1398,8 @@ func GetCCVIDetails(db *sql.DB) {
 	fmt.Println("Created Table for CCVI Data")
 
 	// Fetch data
-	var url = "https://data.cityofchicago.org/resource/9mrw-3jzs.json?$limit=50"
+	var url = "https://data.cityofchicago.org/resource/xhc6-88s9.json?$limit=50"
+
 	res, err := http.Get(url)
 	if err != nil {
 		log.Fatalf("Error fetching CCVI data: %v", err)
