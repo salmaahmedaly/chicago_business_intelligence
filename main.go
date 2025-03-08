a681c8db3bb5cad8a53ad1f79ff637cb546f4481
salmaahmedaly <salma.ahmedaly@outlook.com>
2025-03-08 14:19:40 -0600

    updating business permit

--- main.go
+++ main.go
@@ -129,46 +129,13 @@ type UnemploymentJsonRecords []struct {
 }
 
 type BuildingPermitsJsonRecords []struct {
-	Id                     string `json:"id"`
-	Permit_Code            string `json:"permit_"`
-	Permit_type            string `json:"permit_type"`
-	Review_type            string `json:"review_type"`
-	Application_start_date string `json:"application_start_date"`
-	Issue_date             string `json:"issue_date"`
-	Processing_time        string `json:"processing_time"`
-	Street_number          string `json:"street_number"`
-	Street_direction       string `json:"street_direction"`
-	Street_name            string `json:"street_name"`
-	Suffix                 string `json:"suffix"`
-	Work_description       string `json:"work_description"`
-	Building_fee_paid      string `json:"building_fee_paid"`
-	Zoning_fee_paid        string `json:"zoning_fee_paid"`
-	Other_fee_paid         string `json:"other_fee_paid"`
-	Subtotal_paid          string `json:"subtotal_paid"`
-	Building_fee_unpaid    string `json:"building_fee_unpaid"`
-	Zoning_fee_unpaid      string `json:"zoning_fee_unpaid"`
-	Other_fee_unpaid       string `json:"other_fee_unpaid"`
-	Subtotal_unpaid        string `json:"subtotal_unpaid"`
-	Building_fee_waived    string `json:"building_fee_waived"`
-	Zoning_fee_waived      string `json:"zoning_fee_waived"`
-	Other_fee_waived       string `json:"other_fee_waived"`
-	Subtotal_waived        string `json:"subtotal_waived"`
-	Total_fee              string `json:"total_fee"`
-	Contact_1_type         string `json:"contact_1_type"`
-	Contact_1_name         string `json:"contact_1_name"`
-	Contact_1_city         string `json:"contact_1_city"`
-	Contact_1_state        string `json:"contact_1_state"`
-	Contact_1_zipcode      string `json:"contact_1_zipcode"`
-	Reported_cost          string `json:"reported_cost"`
-	Pin1                   string `json:"pin1"`
-	Pin2                   string `json:"pin2"`
-	Community_area         string `json:"community_area"`
-	Census_tract           string `json:"census_tract"`
-	Ward                   string `json:"ward"`
-	Xcoordinate            string `json:"xcoordinate"`
-	Ycoordinate            string `json:"ycoordinate"`
-	Latitude               string `json:"latitude"`
-	Longitude              string `json:"longitude"`
+	PermitID      string `json:"id"`      // permit_id
+	PermitCode    string `json:"permit_"` // permit_code
+	PermitType    string `json:"permit_type"`
+	TotalFee      string `json:"total_fee"`
+	CommunityArea string `json:"community_area"`
+	Latitude      string `json:"latitude"`
+	Longitude     string `json:"longitude"`
 }
 
 type CovidJsonRecords []struct {
@@ -272,9 +239,9 @@ func main() {
 		// build and fine-tune the functions to pull data from the different data sources
 		// The following code snippets show you how to pull data from different data sources
 
-		go GetCommunityAreaUnemployment(db)
+		// go GetCommunityAreaUnemployment(db)
 		go GetBuildingPermits(db)
-		go GetTaxiTrips(db)
+		// go GetTaxiTrips(db)
 
 		// go GetCovidDetails(db)
 		// go GetCCVIDetails(db)
@@ -497,6 +464,10 @@ func GetTaxiTrips(db *sql.DB) {
 			pickup_zip_code = pickup_address.PostalCode
 		}
 
+		if pickup_zip_code == "" {
+			continue
+		}
+
 		// Using dropoff_centroid_latitude and dropoff_centroid_longitude in geocoder.GeocodingReverse
 		// we could find the dropoff zip-code
 
@@ -517,6 +488,10 @@ func GetTaxiTrips(db *sql.DB) {
 			dropoff_address := dropoff_address_list[0]
 			dropoff_zip_code = dropoff_address.PostalCode
 
+		}
+
+		if dropoff_zip_code == "" {
+			continue
 		}
 
 		sql := `INSERT INTO taxi_trips ("trip_id", "trip_start_timestamp", "trip_end_timestamp", "pickup_centroid_latitude", "pickup_centroid_longitude", "dropoff_centroid_latitude", "dropoff_centroid_longitude", "pickup_zip_code", 
@@ -790,48 +765,14 @@ func GetBuildingPermits(db *sql.DB) {
 	}
 
 	create_table := `CREATE TABLE IF NOT EXISTS "building_permits" (
-						"id"   SERIAL , 
-						"permit_id" VARCHAR(255) UNIQUE, 
-						"permit_code" VARCHAR(255), 
-						"permit_type" VARCHAR(255),  
-						"review_type"      VARCHAR(255), 
-						"application_start_date"      VARCHAR(255), 
-						"issue_date"      VARCHAR(255), 
-						"processing_time"      VARCHAR(255), 
-						"street_number"      VARCHAR(255), 
-						"street_direction"      VARCHAR(255), 
-						"street_name"      VARCHAR(255), 
-						"suffix"      VARCHAR(255), 
-						"work_description"      TEXT, 
-						"building_fee_paid"      VARCHAR(255), 
-						"zoning_fee_paid"      VARCHAR(255), 
-						"other_fee_paid"      VARCHAR(255), 
-						"subtotal_paid"      VARCHAR(255), 
-						"building_fee_unpaid"      VARCHAR(255), 
-						"zoning_fee_unpaid"      VARCHAR(255), 
-						"other_fee_unpaid"      VARCHAR(255), 
-						"subtotal_unpaid"      VARCHAR(255), 
-						"building_fee_waived"      VARCHAR(255), 
-						"zoning_fee_waived"      VARCHAR(255), 
-						"other_fee_waived"      VARCHAR(255), 
-						"subtotal_waived"      VARCHAR(255), 
-						"total_fee"      VARCHAR(255), 
-						"contact_1_type"      VARCHAR(255), 
-						"contact_1_name"      VARCHAR(255), 
-						"contact_1_city"      VARCHAR(255), 
-						"contact_1_state"      VARCHAR(255), 
-						"contact_1_zipcode"      VARCHAR(255), 
-						"reported_cost"      VARCHAR(255), 
-						"pin1"      VARCHAR(255), 
-						"pin2"      VARCHAR(255), 
-						"community_area"      VARCHAR(255), 
-						"census_tract"      VARCHAR(255), 
-						"ward"      VARCHAR(255), 
-						"xcoordinate"      DOUBLE PRECISION ,
-						"ycoordinate"      DOUBLE PRECISION ,
-						"latitude"      DOUBLE PRECISION ,
-						"longitude"      DOUBLE PRECISION,
-						PRIMARY KEY ("id") 
+						"permit_id" VARCHAR(255) UNIQUE,
+						"permit_type" VARCHAR(255),
+						"permit_code" VARCHAR(255),
+						"total_fee" VARCHAR(255),
+						"latitude" VARCHAR(255),
+						"longitude" VARCHAR(255),
+						"community_area" VARCHAR(255),
+						PRIMARY KEY ("permit_id")
 					);`
 
 	_, _err := db.Exec(create_table)
@@ -843,7 +784,7 @@ func GetBuildingPermits(db *sql.DB) {
 
 	// While doing unit-testing keep the limit value to 500
 	// later you could change it to 1000, 2000, 10,000, etc.
-	var url = "https://data.cityofchicago.org/resource/building-permits.json?$limit=500"
+	var url = "https://data.cityofchicago.org/resource/building-permits.json?$limit=100"
 
 	tr := &http.Transport{
 		MaxIdleConns:       10,
@@ -873,165 +814,26 @@ func GetBuildingPermits(db *sql.DB) {
 		// There are different methods to deal with messy/dirty/missing data.
 		// We will use the simplest method: drop records that have messy/dirty/missing data
 		// Any record that has messy/dirty/missing data we don't enter it in the data lake/table
-
-		permit_id := building_data_list[i].Id
+		fmt.Println("Building Permits: Processing Record Number: ", i)
+		permit_id := building_data_list[i].PermitID
 		if permit_id == "" {
 			continue
 		}
 
-		permit_code := building_data_list[i].Permit_Code
-		if permit_code == "" {
-			continue
-		}
-
-		permit_type := building_data_list[i].Permit_type
+		permit_type := building_data_list[i].PermitType
 		if permit_type == "" {
 			continue
 		}
 
-		review_type := building_data_list[i].Review_type
-		if review_type == "" {
+		permit_code := building_data_list[i].PermitCode
+		if permit_code == "" {
 			continue
 		}
 
-		application_start_date := building_data_list[i].Application_start_date
-		if application_start_date == "" {
-			continue
-		}
-		issue_date := building_data_list[i].Issue_date
-		if issue_date == "" {
-			continue
-		}
-		processing_time := building_data_list[i].Processing_time
-		if processing_time == "" {
-			continue
-		}
-
-		street_number := building_data_list[i].Street_number
-		if street_number == "" {
-			continue
-		}
-		street_direction := building_data_list[i].Street_direction
-		if street_direction == "" {
-			continue
-		}
-		street_name := building_data_list[i].Street_name
-		if street_name == "" {
-			continue
-		}
-		suffix := building_data_list[i].Suffix
-		if suffix == "" {
-			continue
-		}
-		work_description := building_data_list[i].Work_description
-		if work_description == "" {
-			continue
-		}
-		building_fee_paid := building_data_list[i].Building_fee_paid
-		if building_fee_paid == "" {
-			continue
-		}
-		zoning_fee_paid := building_data_list[i].Zoning_fee_paid
-		if zoning_fee_paid == "" {
-			continue
-		}
-		other_fee_paid := building_data_list[i].Other_fee_paid
-		if other_fee_paid == "" {
-			continue
-		}
-		subtotal_paid := building_data_list[i].Subtotal_paid
-		if subtotal_paid == "" {
-			continue
-		}
-		building_fee_unpaid := building_data_list[i].Building_fee_unpaid
-		if building_fee_unpaid == "" {
-			continue
-		}
-		zoning_fee_unpaid := building_data_list[i].Zoning_fee_unpaid
-		if zoning_fee_unpaid == "" {
-			continue
-		}
-		other_fee_unpaid := building_data_list[i].Other_fee_unpaid
-		if other_fee_unpaid == "" {
-			continue
-		}
-		subtotal_unpaid := building_data_list[i].Subtotal_unpaid
-		if subtotal_unpaid == "" {
-			continue
-		}
-		building_fee_waived := building_data_list[i].Building_fee_waived
-		if building_fee_waived == "" {
-			continue
-		}
-		zoning_fee_waived := building_data_list[i].Zoning_fee_waived
-		if zoning_fee_waived == "" {
-			continue
-		}
-		other_fee_waived := building_data_list[i].Other_fee_waived
-		if other_fee_waived == "" {
-			continue
-		}
-
-		subtotal_waived := building_data_list[i].Subtotal_waived
-		if subtotal_waived == "" {
-			continue
-		}
-		total_fee := building_data_list[i].Total_fee
+		total_fee := building_data_list[i].TotalFee
 		if total_fee == "" {
 			continue
 		}
-
-		contact_1_type := building_data_list[i].Contact_1_type
-		if contact_1_type == "" {
-			continue
-		}
-
-		contact_1_name := building_data_list[i].Contact_1_name
-		if contact_1_name == "" {
-			continue
-		}
-
-		contact_1_city := building_data_list[i].Contact_1_city
-		if contact_1_city == "" {
-			continue
-		}
-		contact_1_state := building_data_list[i].Contact_1_state
-		if contact_1_state == "" {
-			continue
-		}
-
-		contact_1_zipcode := building_data_list[i].Contact_1_zipcode
-		if contact_1_zipcode == "" {
-			continue
-		}
-
-		reported_cost := building_data_list[i].Reported_cost
-		if reported_cost == "" {
-			continue
-		}
-
-		pin1 := building_data_list[i].Pin1
-		if pin1 == "" {
-			continue
-		}
-
-		pin2 := building_data_list[i].Pin2
-
-		community_area := building_data_list[i].Community_area
-
-		census_tract := building_data_list[i].Census_tract
-		if census_tract == "" {
-			continue
-		}
-
-		ward := building_data_list[i].Ward
-		if ward == "" {
-			continue
-		}
-
-		xcoordinate := building_data_list[i].Xcoordinate
-
-		ycoordinate := building_data_list[i].Ycoordinate
 
 		latitude := building_data_list[i].Latitude
 		if latitude == "" {
@@ -1042,88 +844,25 @@ func GetBuildingPermits(db *sql.DB) {
 		if longitude == "" {
 			continue
 		}
+		community_area := building_data_list[i].CommunityArea
+		if community_area == "" {
+			continue
+		}
 
-		sql := `INSERT INTO building_permits ("permit_id", "permit_code", "permit_type","review_type",
-		"application_start_date",
-		"issue_date",
-		"processing_time",
-		"street_number",
-		"street_direction",
-		"street_name",
-		"suffix",
-		"work_description",
-		"building_fee_paid",
-		"zoning_fee_paid",
-		"other_fee_paid",
-		"subtotal_paid",
-		"building_fee_unpaid",
-		"zoning_fee_unpaid",
-		"other_fee_unpaid",
-		"subtotal_unpaid",
-		"building_fee_waived",
-		"zoning_fee_waived",
-		"other_fee_waived",
-		"subtotal_waived",
-		"total_fee",
-		"contact_1_type",
-		"contact_1_name",
-		"contact_1_city",
-		"contact_1_state",
-		"contact_1_zipcode",
-		"reported_cost",
-		"pin1",
-		"pin2",
-		"community_area",
-		"census_tract",
-		"ward",
-		"xcoordinate",
-		"ycoordinate",
-		"latitude",
-		"longitude" )
-		values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10,$11, $12, $13, $14, $15,$16, $17, $18, $19, $20,$21, $22, $23, $24, $25,$26, $27, $28, $29,$30,$31, $32, $33, $34, $35,$36, $37, $38, $39, $40)`
+		// print something to see the data
+		fmt.Println(permit_id, permit_type, permit_code, total_fee, latitude, longitude, community_area)
+
+		sql := `INSERT INTO building_permits ("permit_id", "permit_type", "permit_code", "total_fee", "latitude", "longitude", "community_area") values($1, $2, $3, $4, $5, $6, $7)`
 
 		_, err = db.Exec(
 			sql,
 			permit_id,
-			permit_code,
 			permit_type,
-			review_type,
-			application_start_date,
-			issue_date,
-			processing_time,
-			street_number,
-			street_direction,
-			street_name,
-			suffix,
-			work_description,
-			building_fee_paid,
-			zoning_fee_paid,
-			other_fee_paid,
-			subtotal_paid,
-			building_fee_unpaid,
-			zoning_fee_unpaid,
-			other_fee_unpaid,
-			subtotal_unpaid,
-			building_fee_waived,
-			zoning_fee_waived,
-			other_fee_waived,
-			subtotal_waived,
+			permit_code,
 			total_fee,
-			contact_1_type,
-			contact_1_name,
-			contact_1_city,
-			contact_1_state,
-			contact_1_zipcode,
-			reported_cost,
-			pin1,
-			pin2,
-			community_area,
-			census_tract,
-			ward,
-			xcoordinate,
-			ycoordinate,
 			latitude,
-			longitude)
+			longitude,
+			community_area)
 
 		if err != nil {
 			panic(err)
@@ -1132,6 +871,8 @@ func GetBuildingPermits(db *sql.DB) {
 	}
 
 	fmt.Println("Completed Inserting Rows into the Building Permits Table")
+	// number of rows inserted into the building_permits table
+	fmt.Println("Number of rows inserted into the building_permits table: ", len(building_data_list))
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
