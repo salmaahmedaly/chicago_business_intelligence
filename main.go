8a9ae970929ff676a7d274e34f8c7195a9be9fcb
salmaahmedaly <salma.ahmedaly@outlook.com>
2025-03-08 11:47:57 -0600

    adding gc project

--- main.go
+++ main.go
@@ -224,7 +224,7 @@ func init() {
 
 	//Option 4
 	//Database application running on Google Cloud Platform.
-	db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=/cloudsql/ADD_YOUR_CONNECTION_NAME_FROM_GCP sslmode=disable port = 5432"
+	db_connection := "user=postgres dbname=chicago_business_intelligence password=root host=/cloudsql/chicago-business-intel:us-central1:mypostgres sslmode=disable port = 5432"
 
 	db, err = sql.Open("postgres", db_connection)
 	if err != nil {
